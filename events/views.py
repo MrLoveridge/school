070617f892ef3b5a29574933ecfc49ff68245db7
MrLoveridge <mark.loveridge1@gmail.com>
2021-09-24 10:01:37 +1200

    Project setup and database creation

--- events/views.py
+++ events/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    #code here
+    return HttpResponse("Hi you have reached the index page")
--- school/settings.py
+++ school/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #appname.apps.AppnameConfig
+    'events.apps.EventsConfig'
 ]
 
 MIDDLEWARE = [
