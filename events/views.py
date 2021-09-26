3d0caec2482087b9a0eb8135637c7cfc3a551b3d
MrLoveridge <mark.loveridge1@gmail.com>
2021-09-27 12:58:44 +1300

    Added filtered queries

--- events/views.py
+++ events/views.py
@@ -1,13 +1,17 @@
+from datetime import datetime, timedelta
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Event
 
 # Create your views here.
 
 def index(request):
 
-    all_events = Event.objects.all()
+    all_events = Event.objects.all().order_by('date')
 
-    context = {'all_events':all_events}
+    events_DTG = all_events.filter(title__contains = 'DTG')
+
+    events_today = all_events.filter(date = datetime.now())
+
+    context = {'all_events':all_events, 'events_DTG':events_DTG, 'events_today':events_today}
     
     return render(request, 'events/index.html', context)
