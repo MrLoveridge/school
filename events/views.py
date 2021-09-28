8b42ebce4f7c6c948d231b2259d96b6e887aa566
MrLoveridge <mark.loveridge1@gmail.com>
2021-09-28 14:39:42 +1300

    Added a detail page view

--- events/views.py
+++ events/views.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Event
 
 # Create your views here.
@@ -8,10 +8,16 @@ def index(request):
 
     all_events = Event.objects.all().order_by('date')
 
-    events_DTG = all_events.filter(title__contains = 'DTG')
-
     events_today = all_events.filter(date = datetime.now())
 
-    context = {'all_events':all_events, 'events_DTG':events_DTG, 'events_today':events_today}
+    context = {'all_events':all_events, 'events_today':events_today}
     
     return render(request, 'events/index.html', context)
+
+def detail(request, event_id):
+
+    event = get_object_or_404(Event, pk=event_id)
+
+    context = {'event': event}
+
+    return render(request, 'events\detail.html', context)
